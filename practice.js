db6701253d05944aa8937f7ad8d325357a41d958
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-05-24 19:50:09 -0700

    Finish!!

---
+++ practice.js
@@ -0,0 +1,52 @@
+let student = {
+    //attr or properties
+    name:"Eduardo",
+    age:23,
+    isActive:true,
+    //function or methods
+    sayHi:function(){
+        return "Hello!";
+    },
+    hobbies:["Football","Listen music","Play Zelda"],  //Array
+    address:{
+        street:"Federacion Croc",
+        zip:"22625",
+        city:"Tijuana",
+        country:"MEX"
+    }
+}
+//display on the document
+document.write(student.address.city,student.address.country,student.address.zip);
+
+//display on the console
+console.log(student.address.zip);
+
+let product1={
+    name:"Laptop",
+    description:"HP Laptop 14'' core i7",
+    price:200
+}
+
+let product2={
+    name:"TV",
+    description:"Sony 32'' UHD",
+    price:300
+}
+
+let product3={
+    name:"Phone",
+    description:"Samsung s21Ultra 128gb",
+    price:250
+}
+/* products = [product1,product2,product3]; */
+products.push(product1,product2,product3);
+/* console.log(products);
+console.log(product1,product2,product3); */
+
+//products.push[product1,product2,product3];
+
+function displayNames(){
+    for(let i=0;i<products.length;i++){
+        console.log(products[i].description);
+    }
+}
---
+++ register.js
@@ -0,0 +1,62 @@
+//object literal
+let salon={
+    name:"the Fashon Pet",
+    address:{
+        street:"Federacion Croc",
+        zip:"22625",
+        city:"Tijuana",
+        number:"332-4"
+    },
+    hours:{
+        open:"9:00 a.m.",
+        close:"5:00 p.m.",
+    },
+    phone:"664-285-84-40",
+    pets:[
+        {
+            name:"Scooby",
+            age:50,
+            gender:"Male",
+            breed:"Dane",
+            service:"Grooming",
+            owner:"Shaggy",
+            phone:"888-888-888"
+        },
+        {
+            name:"moon",
+            age:10,
+            gender:"Female",
+            breed:"Poodle",
+            service:"Grooming",
+            owner:"Luis",
+            phone:"999-999-999"
+        },
+        {
+            name:"Rocky",
+            age:32,
+            gender:"Male",
+            breed:"Chihuahua",
+            service:"Nails court",
+            owner:"Juan",
+            phone:"777-777-777"
+        }
+    ]
+}
+
+console.log(salon.pets[0]);//Displaying scooby
+
+function displaySalonInfo(){
+    let tmp=`
+    <p>Welcome to the ${salon.name} located in ${salon.address.street}</p>
+    <p> </p>`;
+    document.getElementById("info").innerHTML=tmp;
+}
+displaySalonInfo();
+
+function displayName(){
+    for(let i=0;i<salon.pets.length;i++){
+        console.log(salon.pets[i].name);
+    }
+}
+
+displayName();
