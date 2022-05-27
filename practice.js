4dca52d4b8abc7d740fded21932a0ed6e12b48ef
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-05-26 20:41:33 -0700

    Class2

--- practice.js
+++ practice.js
@@ -16,12 +16,12 @@ let student = {
     }
 }
 //display on the document
-document.write(student.address.city,student.address.country,student.address.zip);
+/* document.write(student.address.city,student.address.country,student.address.zip); */
 
 //display on the console
-console.log(student.address.zip);
+//console.log(student.address.zip);
 
-let product1={
+/* let product1={
     name:"Laptop",
     description:"HP Laptop 14'' core i7",
     price:200
@@ -37,9 +37,20 @@ let product3={
     name:"Phone",
     description:"Samsung s21Ultra 128gb",
     price:250
-}
+} */
 /* products = [product1,product2,product3]; */
-products.push(product1,product2,product3);
+function Product(n,d,p){
+    this.name=n;
+    this.description=d;
+    this.price=p;
+}
+
+let product1 = new Product("Speaker","Sony wireless",20.50);
+let product2 = new Product("Laptop","HP",100.00);
+let product3 = new Product("Tv","Samsumng",250.00);
+let product4 = new Product("WebCam","Logitech",15.00);
+
+products.push(product1,product2,product3,product4);
 /* console.log(products);
 console.log(product1,product2,product3); */
 
@@ -50,3 +61,5 @@ function displayNames(){
         console.log(products[i].description);
     }
 }
+
+//displayNames();
--- register.js
+++ register.js
@@ -12,38 +12,10 @@ let salon={
         close:"5:00 p.m.",
     },
     phone:"664-285-84-40",
-    pets:[
-        {
-            name:"Scooby",
-            age:50,
-            gender:"Male",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"Shaggy",
-            phone:"888-888-888"
-        },
-        {
-            name:"moon",
-            age:10,
-            gender:"Female",
-            breed:"Poodle",
-            service:"Grooming",
-            owner:"Luis",
-            phone:"999-999-999"
-        },
-        {
-            name:"Rocky",
-            age:32,
-            gender:"Male",
-            breed:"Chihuahua",
-            service:"Nails court",
-            owner:"Juan",
-            phone:"777-777-777"
-        }
-    ]
+    pets:[]
 }
 
-console.log(salon.pets[0]);//Displaying scooby
+/* console.log(salon.pets[0]);//Displaying scooby */
 
 function displaySalonInfo(){
     let tmp=`
@@ -62,3 +34,91 @@ function displayName(){
 displayName();
 
 alert("There is "+salon.pets.length+" Registers");
+
+function Pet(n,a,g,b,s,o,p){
+    this.name=n;
+    this.age=a;
+    this.gender=g;
+    this.breed=b;
+    this.service=s;
+    this.owner=o;
+    this.phone=p;
+}
+
+
+
+
+let pet1 = new Pet("Scooby",50,"Male","Dane","Grooming","Shaggy","888-888-888");
+let pet2 = new Pet("Moon",10,"Female","Poodle","Grooming","Luis","999-999-999");
+let pet3 = new Pet("Rocky",32,"Male","Chihuahua","Nails Court","Juan","777-777-777");
+
+function register(){
+    console.log("Registering");
+    //get the info from the inputs ***getElementById().value
+    let petName=document.getElementById("txtName").value;
+    let petAge=document.getElementById("txtAge").value;
+    let petGender=document.getElementById("txtGender").value;
+    let petBreed=document.getElementById("txtBreed").value;
+    let petServices=document.getElementById("selService").value;
+    let petOwner=document.getElementById("txtOwner").value;
+    let petPhone=document.getElementById("txtPhone").value;
+    //create the object using the constructor
+    let newPet = new Pet(petName,petAge,petGender,petBreed,petServices,petOwner,petPhone);
+    //push the object in on the array
+    salon.pets.push(newPet);
+    //display it on the console
+    console.log(newPet);
+    console.log(salon.pets);
+    clearForm();
+}
+
+function clearForm(){
+    document.getElementById("txtName").value="";
+    document.getElementById("txtAge").value="";
+    document.getElementById("txtGender").value="";
+    document.getElementById("txtBreed").value="";
+    document.getElementById("selService").value="";
+    document.getElementById("txtOwner").value="";
+    document.getElementById("txtPhone").value="";
+}
+
+function init(){
+    //hook events, triggered events
+    displayName();
+    console.log(pet1,pet2,pet3);
+}
+
+window.onload=init;
+
+
+
+
+
+/*   PETS[]
+ {
+    name:"Scooby",
+    age:50,
+    gender:"Male",
+    breed:"Dane",
+    service:"Grooming",
+    owner:"Shaggy",
+    phone:"888-888-888"
+},
+{
+    name:"moon",
+    age:10,
+    gender:"Female",
+    breed:"Poodle",
+    service:"Grooming",
+    owner:"Luis",
+    phone:"999-999-999"
+},
+{
+    name:"Rocky",
+    age:32,
+    gender:"Male",
+    breed:"Chihuahua",
+    service:"Nails court",
+    owner:"Juan",
+    phone:"777-777-777"
+} */
